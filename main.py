37d619e4eff662f2f9d0d59a9d9e6d755fbbef8e
Ivan Shishlyannikov <ivan.shishlyannikov@zimad.com>
2022-11-27 22:02:31 +0300

    downgrade to verison 13.0

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ import sqlite3
 
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 from telegram.ext import (
-    Application,
     CommandHandler,
     ContextTypes,
     ConversationHandler,
     MessageHandler,
-    filters,
+    filters, Updater, CallbackContext, Filters,
 )
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -20,7 +18,7 @@ logging.basicConfig(
 )
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def start(update: Update, context: CallbackContext) -> int:
     conn = sqlite3.connect('UsersDb.db')
 
     cur = conn.cursor()
@@ -29,13 +27,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     one_result = cur.fetchone()
 
     if one_result is None:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, как тебя зовут?")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, как тебя зовут?")
         return 0
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, "+str(one_result[0]))
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, "+str(one_result[0]))
 
 
-async def get_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+def get_name(update: Update, context: CallbackContext) -> int:
     """Stores the selected gender and asks for a photo."""
     msg = update.message.text
 
@@ -49,30 +47,34 @@ async def get_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     cur2.execute('INSERT INTO USERS VALUES(?,?)', user)
     conn.commit()
 
-    await update.message.reply_text(
+    update.message.reply_text(
         "Привет,"+msg,
     )
     return ConversationHandler.END
 
 
-async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+def cancel(update: Update, context: CallbackContext) -> int:
     """Cancels and ends the conversation."""
-    user = update.message.from_user
-    await update.message.reply_text(
+    update.message.reply_text(
         "Bye! I hope we can talk again some day."
     )
 
     return ConversationHandler.END
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('5764317978:AAHGYsPsCziPutNohfL2DZNclSDgQlA-8gU').build()
 
-    convHandler = ConversationHandler(entry_points=[MessageHandler(filters.TEXT & ~filters.COMMAND, start)],
+    updater = Updater(token='5764317978:AAHGYsPsCziPutNohfL2DZNclSDgQlA-8gU', use_context=True)
+
+    dispatcher = updater.dispatcher
+
+    convHandler = ConversationHandler(entry_points=[MessageHandler(Filters.text & ~Filters.command, start)],
                                       states={
-                                          0:[MessageHandler(filters.TEXT & ~filters.COMMAND, get_name)]
+                                          0: [MessageHandler(Filters.text & ~Filters.command, get_name)]
                                       },
                                       fallbacks=[CommandHandler('cancel', cancel)])
 
-    application.add_handler(convHandler)
+    dispatcher.add_handler(convHandler)
 
-    application.run_polling()
+    updater.start_polling()
+
+    updater.idle()
